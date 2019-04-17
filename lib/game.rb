99506cdd83f57fcdc42082ebb8d8cc4342b33ea6
iampeternguyen <ptnguyen1987@gmail.com>
2019-04-17 22:04:29 +0700

    basic game functioning. need to clean up and refactor

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,12 @@
 class Game
   attr_reader :dictionary
+  attr_accessor :current_player, :prev_player
 
   def initialize(player1, player2)
     @player1, @player2 = player1, player2
-    @fragment = []
+    @current_player, @prev_player = @player1, @player2
+
+    @fragment = ""
     @dictionary = {}
 
     File.open(__dir__ + "/dictionary.txt") do |f|
@@ -12,31 +15,48 @@ class Game
         @dictionary[line] = true
       end
     end
+
   end
 
   def play_round
-
-  end
-
-  def current_player
-  end
-
-  def prev_player
+    take_turn(@current_player)
   end
 
   def next_player!
-
+    @current_player, @prev_player = @prev_player, @current_player
   end
 
   def take_turn(player)
+    puts "Current fragment: #{@fragment}"
+    puts "Please enter a character"
+    char = gets.chomp.downcase
+    while char.length != 1
+      puts "Invalid input. Please enter only one character"
+      char = gets.chomp.downcase
+    end
+    if valid_play?(char)
+      @fragment += char
+      next_player!
+      play_round
+    end
   end
 
   def valid_play?(string)
+    fragment = @fragment+string
+    @dictionary.any? do |key, value|
+      if key.include?(fragment)
+        if @dictionary[fragment]
+          puts "game over"
+        end
+
+        true
+      end
+    end
   end
 
 end
 
 if __FILE__ == $PROGRAM_NAME
   game = Game.new("1", "2")
-  p game.dictionary
+  game.play_round
 end
