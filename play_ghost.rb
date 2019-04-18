d078b03ad1bb7585f92fb3a52710ecd759640a81
iampeternguyen <ptnguyen1987@gmail.com>
2019-04-18 13:26:06 +0700

    Formatting start screen

--- play_ghost.rb
+++ play_ghost.rb
@@ -2,11 +2,11 @@ require_relative ("./lib/game.rb")
 require_relative ("./lib/player.rb")
 
 if __FILE__ == $PROGRAM_NAME
-  puts "How many players? "
+  print "How many players? "
   num_of_of_players = gets.chomp.to_i
   players = []
   while players.length < num_of_of_players
-    puts "Enter Player #{players.length+1}'s name"
+    print "Enter Player #{players.length+1}'s name: "
     players << Player.new(gets.chomp)
   end
   game = Game.new(players)
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 Ghost Game implementation in Ruby for App Academy Open Project
+
+To run, simply run "ruby play_ghost.rb" from terminal.
+Select the number of players and begin.
